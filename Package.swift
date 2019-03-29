0a0e9032f67ee9ce17a00b01039ee8ddf1a4542e
Tomas Camin <tomas.camin@schibsted.com>
2019-03-30 00:09:26 +0100

    Rewrite package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,16 @@
-//
-//  Package.swift
-//  PerfectTemplate
-//
-//  Created by Kyle Jessup on 4/20/16.
-//	Copyright (C) 2016 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "sbtuitestbrowser",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3)
+    name: "sbtuitestbrowser",
+    dependencies: [
+      .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+    ],
+    targets: [
+      .target(
+          name: "sbtuitestbrowser",
+          dependencies: ["PerfectHTTPServer"])
     ]
 )
