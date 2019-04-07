21ee7f59169c23c72140c4cf582530a7ec947c3d
Tomas Camin <tomas.camin@schibsted.com>
2019-04-07 13:56:30 +0200

    Add extension to encode string with html entities

--- Sources/sbtuitestbrowser/Extensions/String+Escape.swift
+++ Sources/sbtuitestbrowser/Extensions/String+Escape.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension String {
-    var unescaped: String {
+    func unescaped() -> String {
         let entities = ["\0", "\t", "\n", "\r", "\"", "\'", "\\"]
         guard var current = self.removingPercentEncoding else {
             return self
@@ -19,6 +19,18 @@ extension String {
             current = current.replacingOccurrences(of: description, with: entity)
         }
         
-        return current.replacingOccurrences(of: "\\/", with: "/")
+        return current
+            .replacingOccurrences(of: "\\/", with: "/")
+            .replacingOccurrences(of: "\n", with: "<br/>")
+    }
+    
+    func encodeUsingHtmlEntities() -> String {
+        return self
+            .replacingOccurrences(of: "&", with: "&amp;")
+            .replacingOccurrences(of: "'", with: "&apos;")
+            .replacingOccurrences(of: "\"", with: "&quot;")
+            .replacingOccurrences(of: "<", with: "&lt;")
+            .replacingOccurrences(of: ">", with: "&gt;")
+            .replacingOccurrences(of: "\n", with: "<br />")
     }
 }
